ef5722ebce9333ae6bea769079723185c7e8e3e3
Anish Rachcha <anishrachcha@gmail.com>
2024-01-04 23:21:48 +0530

    'travel'

--- Self/travel-list/src/App.js
+++ Self/travel-list/src/App.js
@@ -1,6 +1,14 @@
+import { useState } from "react";
 
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+];
 
 export default function App() {
+
+  const [items,setItems]=useState([])
+
   return (
     <div className="App">
       <Logo/>
@@ -18,17 +26,40 @@ function Logo(){
 }
 
 function Form(){
-  return <div className="add-form">
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+  }
+  const [description,setDescription]=useState('')
+  const [quantity,setQuantity]=useState(5)
+  return <form className="add-form" onSubmit={handleSubmit}>
     <h3>What do you need for your trip</h3>
-  </div>
+    <select value={quantity} onChange={(e)=>{setQuantity(Number(e.target.value))}}>
+      {Array.from({length:20},(_,i)=>i+1).map(num=><option value={num} key={num}>{num}</option>)}
+    </select>
+    <input type="text" placeholder="Item..." value={description} onChange={(e)=>setDescription(e.target.value)}></input>
+    <button>Add</button>
+  </form>
 }
 
 function PackagingList(){
-  return <div className="list">LIST</div>
+  return <div className="list"> 
+  <ul>
+  {initialItems.map((item)=>(<Item item={item} key={item.id}/>))}
+  </ul>
+  </div>
+}
+
+function Item({item,id}){
+  return <li>
+    <span style={item.packed?{textDecoration:'line-through'}:{}}>
+    {item.quantity}  {item.description}
+    </span>
+    <button>🧩</button>
+    </li>
 }
 
 function Stats(){
-  return <footer>
+  return <footer className="stats">
     You have x items on your list, and you have packed x(x%)
   </footer>
 }
